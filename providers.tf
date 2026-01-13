3411e77182f10c420d353cff63318982990a88c3
equiroz1995 <elizabethgaete1995@gmail.com>
2026-01-13 17:53:58 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,4 +10,8 @@ terraform {
 provider "azurerm" {
   version = "=3.110.0"
   subscription_id = var.subscription_id
+  features {}
+  skip_provider_registration = "true"
+  storage_use_azuread        = true
+  use_msi = true  # Solo descomenta si ejecutas en Azure con Managed Identity habilitada  
 }  
